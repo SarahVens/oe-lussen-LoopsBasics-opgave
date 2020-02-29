71c895dc3739bfad0a78a178eab34e26f10d783c
maximlesy <maximlesy@gmail.com>
2020-02-29 12:45:12 +0100

    Add project files.

---
+++ LoopsBasics.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoopsBasics.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnWhile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /* Verander count in BtnWhile_Click én in BtnDoWhile_Click naar een startwaarde
+        van 12. Is er een verschil? */
+        private void BtnDoWhile_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnEven_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnForArray_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnForList_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnForeach_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBreak_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnContinue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
